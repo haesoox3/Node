36034deef9d0b54a7617035a2e68c7afda9acefb
Monica Choe <monicchoe@gmail.com>
2017-06-12 15:13:05 -0400

    wc sort

--- bash.js
+++ bash.js
@@ -1,16 +1,37 @@
+// var commands = require('./commands');
+
+// // Output a prompt
+// var prompt = 'prompt > ';
+// process.stdout.write(prompt);
+
+// // The stdin 'data' event fires after a user types in a line
+// process.stdin.on('data', function (data) {
+// 	var cmdString = d.toString().trim();
+// 	var cmdList = cmdString.split(/\s*\|\s*/g);
+// });
+
+// var done = function(output){
+// 	process.stdout.write(output);
+// 	process.stdout.write('\n'+prompt);
+// }
+
 var commands = require('./commands');
 
 // Output a prompt
-var prompt = 'prompt > ';
+var prompt = '\nprompt > ';
 process.stdout.write(prompt);
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim().split(" "); // remove the newline
-  commands[cmd[0]](cmd.splice(1), done);
+	var cmd = data.toString().trim().split(" "); // remove the newline
+	if (commands[cmd[0]]){
+		commands[cmd[0]](cmd.splice(1), done);
+	}
+	else {
+		done("Command not found");
+	}
 });
 
 var done = function(output){
-	process.stdout.write(output);
-	process.stdout.write('\n'+prompt);
+	process.stdout.write(output+prompt);
 }
--- commands.js
+++ commands.js
@@ -1,3 +1,82 @@
+// var fs = require('fs');
+// var request = require('request');
+
+// module.exports = {
+// 	pwd: pwd,
+// 	date: date,
+// 	ls: ls, 
+// 	echo: echo,
+// 	cat: cat,
+// 	head: head,
+// 	tail: tail,
+// 	curl: curl
+// }
+
+// function pwd(stdin, file ,done){
+// 	done(process.cwd());
+// }
+
+// function date(stdin, file, done){
+// 	done((new Date()).toString());
+// }
+
+// function ls(stdin, file, done){
+// 	var output = "";
+// 	fs.readdir('.', function(err, files) {
+// 		files.forEach(function(file) {
+// 			output += file.toString() + "\n";
+// 		})
+// 		done(output);
+// 	});
+// }
+
+// function cat(stdin, file, done){
+// 	fs.readFile(file[0], "utf8", function read(err, data) {
+// 		if (err) throw err;
+// 		done(data);
+// 	});
+// }
+
+// function echo(stdin, file, done){
+// 	done(file.join(" "));
+// }
+
+// function head(stdin, file,done){
+// 	var count = 0;
+// 	var contents;
+// 	var output = "";  
+// 	fs.readFile(file[0], "utf8", function read(err, data) {
+// 		if (err) throw err;
+// 		contents = data.split("\n");
+// 		while (count < 5){
+// 			output += contents[count] + "\n";
+// 			count ++; 
+// 		}
+// 		done(output);
+// 	});
+// }
+
+// function tail(stdin, file,done){
+// 	var count = 0;
+// 	var contents; 
+// 	var output = "";
+// 	fs.readFile(file[0], "utf8", function read(err, data) {
+// 		if (err) throw err;
+// 		contents = data.split("\n");
+// 		while (count < 5){
+// 			output += contents[contents.length-(5-count)] + "\n";
+// 			count ++; 
+// 		}
+// 		done(output);
+// 	});
+// }
+
+// function curl(stdin, file, done){
+// 	request(file[0], function (error, response, body) {
+//   			done(body); // Print the HTML for the Google homepage.
+//   		});
+// }
+
 var fs = require('fs');
 var request = require('request');
 
@@ -9,7 +88,10 @@ module.exports = {
 	cat: cat,
 	head: head,
 	tail: tail,
-	curl: curl
+	curl: curl,
+	sort: sort,
+	wc: wc,
+	uniq: uniq
 }
 
 function pwd(file ,done){
@@ -21,58 +103,55 @@ function date(file, done){
 }
 
 function ls(file, done){
-	var output = "";
 	fs.readdir('.', function(err, files) {
-		files.forEach(function(file) {
-			output += file.toString() + "\n";
-		})
-		done(output);
+		done(files.join("\n"));
 	});
 }
 
-function cat(args, done){
-	fs.readFile(args[0], "utf8", function read(err, data) {
+function echo(file, done){
+	done(file.join(" "));
+}
+
+function cat(file, done){
+	fs.readFile(file[0], "utf8", function read(err, data) {
 		if (err) throw err;
 		done(data);
 	});
 }
 
-function echo(args, done){
-	done(args.join(" "));
-}
-
-function head(args,done){
-	var count = 0;
-	var contents;
-	var output = "";  
-	fs.readFile(args[0], "utf8", function read(err, data) {
+function head(file,done){
+	fs.readFile(file[0], "utf8", function read(err, data) {
 		if (err) throw err;
-		contents = data.split("\n");
-		while (count < 5){
-			output += contents[count] + "\n";
-			count ++; 
-		}
-		done(output);
+		done(data.split("\n").slice(0, 5).join("\n"));
 	});
 }
 
-function tail(args,done){
-	var count = 0;
-	var contents; 
-	var output = "";
-	fs.readFile(args[0], "utf8", function read(err, data) {
+function tail(file,done){
+	fs.readFile(file[0], "utf8", function read(err, data) {
 		if (err) throw err;
-		contents = data.split("\n");
-		while (count < 5){
-			output += contents[contents.length-(5-count)] + "\n";
-			count ++; 
-		}
-		done(output);
+		done(data.split("\n").slice(-5).join("\n"));
 	});
 }
 
-function curl(args, done){
-	request(args[0], function (error, response, body) {
+function curl(file, done){
+	request(file[0], function (error, response, body) {
   			done(body); // Print the HTML for the Google homepage.
   		});
+}
+
+function sort(file, done){
+	fs.readdir('.', function(err, files) {
+		done(files.sort().join("\n"));
+	});
+}
+
+function wc(file, done){
+	fs.readFile(file[0], "utf8", function read(err, data) {
+		if (err) throw err;
+		done(data.split("\n").length);
+	});
+}
+
+function uniq(file, done){
+
 }
